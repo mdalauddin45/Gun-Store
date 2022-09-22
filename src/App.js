42119dc66c9352e894ac8b55777429cc7912c9f3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-22 22:11:38 +0600

    All gun add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import AllGun from './Components/AllGun/AllGun';
 
 function App() {
   return (
     <div className="">
-      <h1>Hello</h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <Navbar></Navbar>
+      <AllGun></AllGun>
     </div>
   );
 }
---
+++ src/Components/AllGun/AllGun.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllGun = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AllGun;

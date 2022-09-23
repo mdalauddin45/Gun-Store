c46cac640fc9a57dd39eed62b5a29c50e70352b9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-09-23 10:31:39 +0600

    Add

--- src/Components/SingleGun/SingleGun.js
+++ src/Components/SingleGun/SingleGun.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
+import Modal from '../Modal/Modal';
 
 const SingleGun = (props) => {
-    console.log(props);
+    const [modal, setModal] = useState({});
     const { gun, countIncrease } = props;
-    console.log(gun);
     const { action, capacity, category, id, img, name, price, bullet } = gun;
 
     return (
@@ -16,15 +17,20 @@ const SingleGun = (props) => {
                         {name}
                         <div className="badge badge-secondary">NEW</div>
                     </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>ID: {id} <br /> Capacity: {capacity} <br /> Category: {category} <br /> Price: {price}</p>
                     <div className="card-actions justify-end">
                         <div className="badge badge-outline">{action}</div>
                         <div className="badge badge-outline">{bullet}</div>
                     </div>
                     <div className="card-actions justify-end">
                         <button onClick={() => countIncrease()} className="btn btn-sm">Add to cart</button>
-                        <button className="btn btn-sm btn-success">Details</button>
+                        <label
+                            onClick={() => setModal(gun)}
+                            htmlFor="my-modal-3" className="btn btn-sm btn-success modal-button">Details</label>
                     </div>
+                    {
+                        modal && <Modal modal={modal} setModal={setModal}></Modal>
+                    }
                 </div>
             </div>
         </div >
